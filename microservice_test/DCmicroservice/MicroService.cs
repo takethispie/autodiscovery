a5e660c728ef62dfcb6b706c2773f0f8ee458db5
takethispie <takethispie@gmail.com>
2016-03-02 00:18:14 +0100

    commit de base

---
+++ microservice_test/DCmicroservice/MicroService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace DCmicroservice
+{
+    public class MicroService
+    {
+
+        Thread thread;
+
+        public virtual string Name { get; set; }
+
+        public virtual byte Status { get; set; }
+
+        public virtual short Id { get; set; }
+
+        //register bus to send data to
+        public event MessageEvent send;
+
+        public virtual void Init()
+        {
+            thread = new Thread(run);
+        }
+
+        //receive data
+        public virtual void OnMessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+
+        }
+
+        public virtual void run()
+        {
+
+        }
+
+        public virtual void Shutdown()
+        {
+
+        }
+    }
+}
---
+++ microservice_test/DCmicroservice/MicroServiceCore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCmicroservice
+{
+    public class MicroServiceCore
+    {
+        Dictionary<string,MicroServiceBus> MicroSBUS;
+
+        public MicroServiceCore()
+        {
+            MicroSBUS = new Dictionary<string,MicroServiceBus>();
+            MicroSBUS.Add("Main",new MicroServiceBus("Main"));
+        }
+    }
+}
